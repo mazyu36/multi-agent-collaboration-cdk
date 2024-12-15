bbfeaab1b143528bc6e51a6840130a3c1fa3a189
maz <yiyth.fcb6@gmail.com>
2024-12-15 15:59:07 +0900

    Initial commit

---
+++ test/workshop_bedrock_multi_agents.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as WorkshopBedrockMultiAgents from '../lib/workshop_bedrock_multi_agents-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/workshop_bedrock_multi_agents-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new WorkshopBedrockMultiAgents.WorkshopBedrockMultiAgentsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
